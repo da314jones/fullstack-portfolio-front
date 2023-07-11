c9dd36e1539f148689189470ab42ba3861ae36c4
da314jones <dwaynejones@pursuit.org>
2023-07-11 19:45:00 -0400

    query adjustments

--- newsfeed.js
+++ newsfeed.js
@@ -1,7 +1,7 @@
 
 //variable for api
-const NEWSAPI_URL = "https://newsapi.org/v2/top-headlines/sources?apiKey=1681be04f001473c982be84269c6cec3";
-
+const NEWSAPI_URL = "https://newsapi.org/v2/sources?apiKey=a9c241399e2d4b609fac5b8b2b293684";
+const API = a9c241399e2d4b609fac5b8b2b293684
 //locate button and 
 // const button = document.querySelector("button");
 const main = document.querySelector("main");
@@ -19,80 +19,90 @@ const main = document.querySelector("main");
 //         })
 // });
 
-document.addEventListener("DOMContentLoaded", function() {
-    console.log("gey Dom Loaded")
-    fetch(`${NEWSAPI_URL}`)
-        .then((response) => response.json())
-        .then((data) => {
-            showNews(data);
-        })
-        .catch((err) => {
-            showError(err);
-        })
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("Dom Loaded")
+    fetchNews();
 });
- 
+
 //event listener for form
-document.getElementById("queryForm").addEventListener("submit", function(e) {
+document.getElementById("queryForm").addEventListener("submit", function (e) {
     e.preventDefault();
 
-    const query = document.getElementById("queryInput").value;
-    const sources = document.getElementById("sourcesInput").value;
+    const category = document.getElementById("categoryInput").value;
+    const country = document.getElementById("countryInput").value;
     const language = document.getElementById("languageInput").value;
+    const name = document.getElementById("nameInput").value;
 
     //query parameters
     const queryParameters = {
-        apiKey: "1681be04f001473c982be84269c6cec3",
-        q: query,
-        sources: sources,
+        apiKey: API,
+        category: category,
+        country: country,
         language: language,
-        sortBy: "publishedAt",
-        pageSize: 20,
-        page: 1,
+        name: name,
     };
 
     // create url
     const queryUrl = createQueryUrl(queryParameters);
 
     //fetch query data from api
-    fetch(queryUrl)
-    .then(response => response.json())
-    .then(data => {
-        showResults(data.articles);
-    })
-    .catch(error => {
-        console.error(error);
-    })
+    fetchNews(queryUrl);
 });
 
-const showNews = (data) => {
+//create url to encoding 
+const createQueryUrl = (params) => {
+    const queryString = Object.entries(params).map(([key, value]) => `${encodeURIComponent(key)}=${encodeURIComponent(value)}`).join('&');
+    return `${NEWSAPI_URL}&${queryString}`;
+}
+
+const fetchNews = (url = NEWSAPI_URL) => {
+    fetch(url)
+        .then((response) => response.json)
+        .then((data) => {
+            console.log(data)
+            showNews(data.articles);
+            document.getElementById("queryForm").reset();
+        })
+        .catch(error => {
+            console.error(error);
+        })
+}
+
+// modify for user query
+const showNews = (sources) => {
+    console.log(data)
     main.innerHTML = "";
 
-    data.sources.forEach((source) => {
+    sources.forEach((source) => {
         const panel = document.createElement("div");
         panel.classList.add("panel");
 
-        const category = document.createElement("p");
-        category.textContent = source.category;
-        panel.appendChild(category);
-
-        const country = document.createElement("h2");
-        country.textContent = source.country;
-        panel.appendChild(country);
-
-        const name = document.createElement("h3");
+        const name = document.createElement("h2");
         name.textContent = source.name;
-        panel.appendChild(name);
+        panel.appendChild(title);
 
         const description = document.createElement("p");
         description.textContent = source.description;
         panel.appendChild(description);
+
+        const category = document.createElement("h3");
+        category.textContent = source.category.toUpperCase();
+        panel.appendChild(category);
+
+        const country = document.createElement("h3");
+        country.textContent = source.country.toUpperCase();
+        panel.appendChild(country);
+
+        const language = document.createElement("p");
+        language.textContent = source.language.toUpperCase();
+        panel.appendChild(language);
 
         const readMore = document.createElement("a");
         readMore.textContent = "Read more";
         readMore.href = source.url;
         readMore.addEventListener("click", openInSameTab);
         panel.appendChild(readMore)
-        
+
         main.appendChild(panel)
     })
 }
