ab90a34805d5b2eebaccbbe687a2c9cb5833bc87
da314jones <dwaynejones@pursuit.org>
2023-07-11 21:54:35 -0400

    Complete functionality

--- newsfeed.js
+++ newsfeed.js
@@ -1,11 +1,14 @@
 
 //variable for api
 const NEWSAPI_URL = "https://newsapi.org/v2/sources?apiKey=a9c241399e2d4b609fac5b8b2b293684";
-const API = "a9c241399e2d4b609fac5b8b2b293684"
+const API_KEY = "a9c241399e2d4b609fac5b8b2b293684"
 //locate button and 
 // const button = document.querySelector("button");
 const main = document.querySelector("main");
-
+const createQueryUrl = (params => {
+    const queryString = new URLSearchParams(params).toString();
+    return `${NEWSAPI_URL}&${queryString}`;
+})
 //fetch and catch eventListener
 // button.addEventListener("click", (e) => {
 //     e.preventDefault();
@@ -31,29 +34,18 @@ document.getElementById("queryForm").addEventListener("submit", function (e) {
     const category = document.getElementById("categoryInput").value;
     const country = document.getElementById("countryInput").value;
     const language = document.getElementById("languageInput").value;
-    const name = document.getElementById("nameInput").value;
 
-    //query parameters
     const queryParameters = {
-        apiKey: API,
+        apiKey: API_KEY,
         category: category,
         country: country,
-        language: language,
-        name: name,
+        language: language
     };
 
-    // create url
     const queryUrl = createQueryUrl(queryParameters);
 
-    //fetch query data from api
     fetchNews(queryUrl);
 });
-
-//create url to encoding 
-const createQueryUrl = (params) => {
-    const queryString = Object.entries(params).map(([key, value]) => `${encodeURIComponent(key)}=${encodeURIComponent(value)}`).join('&');
-    return `${NEWSAPI_URL}?${queryString}`;
-}
 
 const fetchNews = (url = NEWSAPI_URL) => {
     fetch(url)
@@ -68,12 +60,18 @@ const fetchNews = (url = NEWSAPI_URL) => {
         })
 }
 
-// modify for user query
 const showNews = (sources) => {
-    console.log(sources)
+    if (!sources) {
+        console.log('No sources to show');
+        return;
+    }
+
+    // Sort sources by name
+    const sortedSources = sources.sort((a, b) => a.name.localeCompare(b.name));
+    
     main.innerHTML = "";
 
-    sources.forEach((source) => {
+    sortedSources.forEach((source) => {
         const panel = document.createElement("div");
         panel.classList.add("panel");
 
@@ -100,7 +98,7 @@ const showNews = (sources) => {
         const readMore = document.createElement("a");
         readMore.textContent = "Read more";
         readMore.href = source.url;
-        readMore.addEventListener("click", openInSameTab);
+        // readMore.addEventListener("click", openInSameTab);
         panel.appendChild(readMore)
 
         main.appendChild(panel)
@@ -109,7 +107,7 @@ const showNews = (sources) => {
 
 const openInSameTab = (e) => {
     e.preventDefault();
-    window.open(e.target.href, "_blank")
+    window.open(e.target.href, "_self")
 };
 
 const showError = (err) => {
